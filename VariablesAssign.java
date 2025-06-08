6aab0f502b5c24cb3d513db9b86c6a94b63d8d0e
sanjay-1718 <sanjaybhargav1718@gmail.com>
2025-06-09 01:03:48 +0530

    Creating variables from real life examples

---
+++ VariablesAssign.java
@@ -0,0 +1,20 @@
+public class VariablesAssign {
+    public static void main(String[] args) {
+        String brand = "HIGHLANDER";
+        String description = "Men Comfort Relaxed Fit Textured Spread Collar Casual Shirt";
+        float rating = 4.6f;
+        int rate = 750;
+        int MRP = 3949;
+        float discount = 81.00f;
+        byte size = 42;
+
+        System.out.println(brand);
+        System.out.println(description);
+        System.out.println(rating);
+        System.out.println(rate);
+        System.out.println(MRP);
+        System.out.println(discount);
+        System.out.println(size);
+
+    }
+}
---
+++ VariablesAssign1.java
@@ -0,0 +1,28 @@
+public class VariablesAssign1 {
+    public static void main(String[] args) {
+        String brand = "Mahindra Thar";
+        float rating = 4.5f;
+        int reviews = 1400;
+        int rate = 1762000;
+        short engine = 2184;
+        float power = 150.19f;
+        byte seatingCapacity = 4;
+        float globalSafteyRating = 4f;
+        short GroundClearance = 226;
+        short Torque = 320;
+        String DriveType = "4WD/RWD";
+
+        System.out.println(brand);
+        System.out.println(rating);
+        System.out.println(reviews);
+        System.out.println(rate);
+        System.out.println(engine);
+        System.out.println(power);
+        System.out.println(seatingCapacity);
+        System.out.println(globalSafteyRating);
+        System.out.println(GroundClearance);
+        System.out.println(Torque);
+        System.out.println(DriveType);
+
+    }
+}
