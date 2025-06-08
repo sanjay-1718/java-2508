83db0266bea1e1171a5ae679a86aaffa72106c05
sanjay-1718 <sanjaybhargav1718@gmail.com>
2025-06-08 23:15:09 +0530

    defining variables

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        // comments
+        int age = 20;
+        String name = "Sanjay";
+        String message = "Welcome to java Programming";
+        System.out.println(message);
+
+    }
+}
