12219d2e549239361ff77bcc893889b9c1eb7ada
sanjay-1718 <sanjaybhargav1718@gmail.com>
2025-06-08 23:50:08 +0530

    updated variables

--- Variables.java
+++ Variables.java
@@ -1,10 +1,26 @@
 public class Variables {
     public static void main(String[] args) {
         // comments
-        int age = 20;
+        byte byteNumber = 120;
+        short shortNumber = 1000;
+        int intNumber = 459;
+        long longNumber = 20000;
+        float floatNumber = 3.5f;
+        double doubleNumber = 5.76;
+        boolean booleanValue = true; // false
+        char charValue = 'V';
+
         String name = "Sanjay";
         String message = "Welcome to java Programming";
         System.out.println(message);
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(booleanValue);
+        System.out.println(charValue);
 
     }
 }
