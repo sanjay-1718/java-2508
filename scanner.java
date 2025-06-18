bc79c62aef1570b304e7a217451a583554ab4786
sanjay-1718 <sanjaybhargav1718@gmail.com>
2025-06-18 22:39:12 +0530

    Taking Dynamic Input

---
+++ scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class scanner {
+    public static void main(String[] args) {
+        // Input from User
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String name = input.nextLine();
+
+        System.out.println("Enter your Id: ");
+        int id = input.nextInt();
+
+        System.out.println("Hi " +name +" your Id is " +id);
+
+        System.out.println("Work Completed stop taking Input");
+        input.close(); // Closing the input
+    }
+}
