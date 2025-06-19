6345af0ebadf44381036c414464a4993edaabde0
sanjay-1718 <sanjaybhargav1718@gmail.com>
2025-06-19 12:35:03 +0530

    Car Class

---
+++ Car/Car.java
@@ -0,0 +1,46 @@
+package Car;
+
+public class Car {
+    // Data of a car
+    String carBrand;
+    String carModel;
+    double carPrice;
+    String carColor;
+    double carOffers;
+
+    // behaviours of short description of car
+    public void displayCarInfoBanner(){
+        System.out.println("Car Brand: " +carBrand);
+        System.out.println("Car Model: " +carModel);
+        System.out.println("Car Price: " +carPrice);
+    }
+    // behaviours of long description of car
+    public void displayCarInfoPage(){
+        System.out.println("Car Brand: " +carBrand);
+        System.out.println("Car Model: " +carModel);
+        System.out.println("Car Price: " +carPrice);
+        System.out.println("Car Color: " +carColor);
+        System.out.println("Car Offers: " +carOffers);
+
+    }
+    //EMI Calculation data
+    int principleAmount;
+    double annualIntrestRate;
+    byte loanTenureYrs;
+    int downPayment;
+
+    public void displayCarEmi(){
+        double monthlyIntrestRate = (annualIntrestRate/12)/100;
+        int totalMonths = loanTenureYrs*12;
+        double emiIntrest = 1+monthlyIntrestRate;
+
+        System.out.println("Down Payment is: " +downPayment);
+        System.out.println("Total Loan amount is: " +principleAmount);
+
+        double EMI = (principleAmount*monthlyIntrestRate*(Math.pow(emiIntrest, totalMonths)))/(Math.pow(emiIntrest, totalMonths)-1);
+        System.out.println("EMI per month is: " +EMI);
+
+        System.out.println("Total Payable amount is: " +(EMI*totalMonths));
+    }
+}
+
