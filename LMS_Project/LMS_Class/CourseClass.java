61efec385dcb22a51902ae7522f10bfe13b8bf21
sanjay-1718 <sanjaybhargav1718@gmail.com>
2025-06-25 09:29:37 +0530

    Course Class

--- LMS_Project/LMS_Class/CourseClass.java
+++ LMS_Project/LMS_Class/CourseClass.java
@@ -1,10 +1,34 @@
 package LMS_Project.LMS_Class;
 
+import java.util.Scanner;
+
 public class CourseClass {
     // Course Data
     String courseTitle;
     byte courseDuration;
     String trainerName;
+
+    public void courseInput(){
+
+        Scanner sc = new Scanner(System.in);
+
+        String continueInput = "yes";
+        do{
+        System.out.println("Enter Course Title: ");
+        courseTitle = sc.nextLine();
+
+        System.out.println("Enter Course Duration: ");
+        courseDuration = sc.nextByte();
+
+        System.out.println("Enter Trainer Name: ");
+        trainerName = sc.nextLine();
+
+        System.out.println("Do yo want to continue? (yes/no)");
+        continueInput = sc.nextLine();
+
+        } while (continueInput.equals("yes") || continueInput.equals("YES") || continueInput.equals("Yes"));
+        sc.close();
+    }
     
     public void displayCourseDetails(){
         System.out.println("Course title is: " +courseTitle);
